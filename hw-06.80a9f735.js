71ddecfca9f92f28c04c5cad544206c70e6ac232
Kondrov220 <121194027+Kondrov220@users.noreply.github.com>
2025-05-03 18:04:23 +0300

    Updates

---
+++ hw-06.80a9f735.js
@@ -0,0 +1,2 @@
+const t=t=>{document.querySelector("span").textContent=t};document.querySelector("button").addEventListener("click",()=>{t(prompt("Як вас звати?"))});
+//# sourceMappingURL=hw-06.80a9f735.js.map
